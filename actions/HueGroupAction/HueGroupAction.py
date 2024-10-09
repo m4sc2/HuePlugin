8d3159e2d588c5d72cce34ea325480844acb97de
m4sc <mschindler@masc-serv.de>
2024-10-09 20:12:23 +0200

    add get group list
    add method to toggle the light status for the whole group

--- actions/HueGroupAction/HueGroupAction.py
+++ actions/HueGroupAction/HueGroupAction.py
@@ -1,77 +1,77 @@
 # Import StreamController modules
+from src.backend.PluginManager.ActionBase import ActionBase
 # Import python modules
 import os
-
 # Import gtk modules - used for the config rows
 import gi
-
-from src.backend.PluginManager.ActionBase import ActionBase
 
 gi.require_version("Gtk", "4.0")
 gi.require_version("Adw", "1")
 from gi.repository import Adw
+
 from loguru import logger as log
 
 
 class HueGroupAction(ActionBase):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.bridge_ip_entry = None
-        self.bridge_user_entry = None
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    self.bridge_ip_entry = None
+    self.bridge_user_entry = None
+    self.bridge_groups = None
 
-    def on_ready(self) -> None:
-        """
-        initialize the action
-        Returns: None
+  def on_ready(self) -> None:
+    """
+    initialize the action
+    Returns: None
 
-        """
-        icon_path = os.path.join(self.plugin_base.PATH, "assets", "info.png")
-        self.set_media(media_path=icon_path, size=0.75)
-        settings = self.get_settings()
-        self.plugin_base.backend.set_connection_details(settings.get("BRIDGE_IP", ""), settings.get("BRIDGE_USER", ""))
+    """
+    icon_path = os.path.join(self.plugin_base.PATH, "assets", "info.png")
+    self.set_media(media_path=icon_path, size=0.75)
+    settings = self.get_settings()
+    self.plugin_base.backend.set_connection_details(settings.get("BRIDGE_IP", ""), settings.get("BRIDGE_USER", ""))
+    self.bridge_groups = self.plugin_base.backend.get_groups()
 
-    def on_key_down(self) -> None:
-        log.info("Key down")
-        log.info(self.plugin_base.backend.get_test())
-        self.plugin_base.backend.get_groups()
+  def on_key_down(self) -> None:
+    log.info("Key down")
+    self.plugin_base.backend.toggle_group_lights(1)
 
-    def on_key_up(self) -> None:
-        log.info("Key up")
+  def on_key_up(self) -> None:
+    log.info("Key up")
 
-    def get_config_rows(self) -> list:
-        """
-        provides the config entries for the plugin
-        Returns: config entries as list
+  def get_config_rows(self) -> list:
+    """
+    provides the config entries for the plugin
+    Returns: config entries as list
 
-        """
-        self.bridge_ip_entry = Adw.EntryRow(title=self.plugin_base.lm.get("hue.gateway.ip.title"))
-        self.bridge_user_entry = Adw.EntryRow(title=self.plugin_base.lm.get("hue.gateway.username.title"))
+    """
+    self.bridge_ip_entry = Adw.EntryRow(title=self.plugin_base.lm.get("hue.gateway.ip.title"))
+    self.bridge_user_entry = Adw.EntryRow(title=self.plugin_base.lm.get("hue.gateway.username.title"))
 
-        self.load_config_defaults()
+    self.load_config_defaults()
 
-        # Connect signals
-        self.bridge_ip_entry.connect("notify::text", self.on_url_changed)
-        self.bridge_user_entry.connect("notify::text", self.on_json_changed)
+    # Connect signals
+    self.bridge_ip_entry.connect("notify::text", self.on_ip_changed)
+    self.bridge_user_entry.connect("notify::text", self.on_username_changed)
 
-        return [self.bridge_ip_entry, self.bridge_user_entry]
+    return [self.bridge_ip_entry, self.bridge_user_entry]
 
-    def load_config_defaults(self):
-        """
-        loads the already configured values
-        Returns: already configured values or defaults
+  def load_config_defaults(self):
+    """
+    loads the already configured values
+    Returns: already configured values or defaults
 
-        """
-        self.bridge_ip_entry.set_text(self.get_settings().get("BRIDGE_IP", ""))  # Does not accept None
-        self.bridge_user_entry.set_text(self.get_settings().get("BRIDGE_USER", ""))  # Does not accept None
+    """
+    self.bridge_ip_entry.set_text(self.get_settings().get("BRIDGE_IP", ""))  # Does not accept None
+    self.bridge_user_entry.set_text(self.get_settings().get("BRIDGE_USER", ""))  # Does not accept None
 
-    def on_url_changed(self, entry, *args):
-        settings = self.get_settings()
-        settings["BRIDGE_IP"] = entry.get_text()
-        self.set_settings(settings)
-        self.plugin_base.backend.set_connection_details(settings.get("BRIDGE_IP", ""), settings.get("BRIDGE_USER", ""))
+  def on_ip_changed(self, entry, *args):
+    settings = self.get_settings()
+    settings["BRIDGE_IP"] = entry.get_text()
+    self.set_settings(settings)
+    self.plugin_base.backend.set_connection_details(settings.get("BRIDGE_IP", ""), settings.get("BRIDGE_USER", ""))
 
-    def on_json_changed(self, entry, *args):
-        settings = self.get_settings()
-        settings["BRIDGE_USER"] = entry.get_text()
-        self.set_settings(settings)
-        self.plugin_base.backend.set_connection_details(settings.get("BRIDGE_IP", ""), settings.get("BRIDGE_USER", ""))
+  def on_username_changed(self, entry, *args):
+    settings = self.get_settings()
+    settings["BRIDGE_USER"] = entry.get_text()
+    self.set_settings(settings)
+    self.plugin_base.backend.set_connection_details(settings.get("BRIDGE_IP", ""), settings.get("BRIDGE_USER", ""))
--- backend/hue_assist.py
+++ backend/hue_assist.py
@@ -6,49 +6,72 @@ from loguru import logger as log
 from phue import Bridge
 
 
+class Group:
+  def __init__(self, group_name, status, brightness, group_id):
+    self.group_name = group_name
+    self.status = status
+    self.brightness = brightness
+    self.group_id = group_id
+
+  def __str__(self):
+    return f"{self.group_name}({self.status}) - groupId: {self.group_id} - brightness: {self.brightness}"
+
+
 class HueBackend(BackendBase):
-    def __init__(self):
-        super().__init__()
-        _bridge: Bridge = None
-        _bridge_ip: str = ""
-        _bridge_username: str = ""
+  def __init__(self):
+    super().__init__()
+    self._bridge: Bridge = None
+    self._bridge_ip: str = ""
+    self._bridge_username: str = ""
 
-    def set_connection_details(self, host: str, username: str) -> None:
-        """
-        Initialize the connection to the Hue Bridge.
+  def set_connection_details(self, host: str, username: str) -> None:
+    """
+    Initialize the connection to the Hue Bridge.
 
-        Args:
-            self: the backend
-            username:   username of the Hue Bridge
-            host:       ip address of the Hue Bridge
+    Args:
+        self: the backend
+        username:   username of the Hue Bridge
+        host:       ip address of the Hue Bridge
 
-        Returns: None
+    Returns: None
 
-        """
-        self._bridge_ip = host
-        self._bridge_username = username
-        if self._bridge_ip == "": return
-        if self._bridge_username == "": return
-        self._bridge = Bridge(ip=self._bridge_ip, username=self._bridge_username)
-        log.info(self._bridge.get_api())
+    """
+    self._bridge_ip = host
+    self._bridge_username = username
+    if self._bridge_ip == "": return
+    if self._bridge_username == "": return
+    self._bridge = Bridge(ip=self._bridge_ip, username=self._bridge_username)
+    log.info(self._bridge.get_api())
 
-    @staticmethod
-    def get_test() -> str:
-        return "test"
+  def is_connected(self) -> bool:
+    try:
+      self._bridge.connect()
+      return True
+    except Exception as e:
+      return False
 
-    def is_connected(self) -> bool:
-        try:
-            self._bridge.connect()
-            return True
-        except Exception as e:
-            return False
+  def get_groups(self) -> list:
+    _groupList = []
+    for group in self._bridge.groups:
+      g = Group(group.name, group.on, 100, group.group_id)
+      log.trace(group.name + " " + str(group.on))
+      _groupList.append(g)
+    return _groupList
 
-    def get_groups(self) -> list:
-        for group in self._bridge.groups :
-            log.info(group.name + str(group.on))
-            self._bridge.set_group(group.group_id, 'on', True)
-        return self._bridge.groups
+  def toggle_group_lights(self, group_id: int) -> None:
+    """
+    Toggle lights on the group.
 
+    Args:
+      self: the backend
+      group_id:  id of the group
+
+    Returns:
+      None
+
+    """
+    current_state: bool = self._bridge.get_group(group_id, 'on')
+    self._bridge.set_group(group_id, 'on', not current_state)
 
 
 backend = HueBackend()
